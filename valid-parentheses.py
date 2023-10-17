9f69eb1acd9c911f70efb8b583746e089952227d
Aurora Ignacio <bellaignacio10@gmail.com>
2023-10-17 00:06:56 -0700

    valid parentheses

---
+++ valid-parentheses.py
@@ -0,0 +1,47 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0: # immediately invalid - can't have matching parentheses if length is odd
+            return False
+
+        stack = []
+
+        close_to_open = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for p in s:
+            if p not in close_to_open:
+                stack.append(p) # then p is an open parentheses
+            elif not stack or stack[-1] != close_to_open[p]:
+                return False # if stack has no open parentheses or the most recent one is not p's match
+            else:
+                stack.pop() # then p has a match, clear both
+
+        return len(stack) == 0 # no leftover open parentheses
+
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         if len(s) % 2 != 0:
+#             return False
+
+#         # elif s[0] in (')', '}', ']'):
+#         #     return False
+
+#         else:
+#             open = []
+
+#             for p in s:
+#                 if p in ('(', '{', '['):
+#                     open.append(p)
+#                 elif p == ')' and len(open) > 0 and open[-1] == '(':
+#                     open.pop()
+#                 elif p == '}' and len(open) > 0 and open[-1] == '{':
+#                     open.pop()
+#                 elif p == ']' and len(open) > 0 and open[-1] == '[':
+#                     open.pop()
+#                 else:
+#                     return False
+
+#             return len(open) == 0
