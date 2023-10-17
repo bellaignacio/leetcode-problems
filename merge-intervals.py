1578e3a1a845732fe9d97948012ec3973b71aa51
Aurora Ignacio <bellaignacio10@gmail.com>
2023-10-17 11:57:05 -0700

    merge intervals

--- merge-intervals.py
+++ merge-intervals.py
@@ -8,11 +8,35 @@ class Solution:
         for i in range(1, len(intervals)):
             start, end = intervals[i][0], intervals[i][1]
             if start <= stack[-1][1]: # overlapping
-                if start <= stack[-1][0]:
+                if start < stack[-1][0]:
                     stack[-1][0] = start
-                if end >= stack[-1][1]:
+                if end > stack[-1][1]:
                     stack[-1][1] = end
             else: # not overlapping
                 stack.append([start, end])
 
         return stack
+
+
+# class Solution:
+#     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+#         intervals.sort()
+
+#         stack = [intervals[0]]
+
+#         for i in range(1, len(intervals)):
+#             curr_int = {
+#                 'start': intervals[i][0],
+#                 'end': intervals[i][1]
+#             }
+#             last_int = {
+#                 'start': stack[-1][0],
+#                 'end': stack[-1][1]
+#             }
+
+#             if curr_int['start'] <= last_int['end'] and curr_int['end'] > last_int['end']:
+#                 stack[-1][1] = curr_int['end']
+#             elif curr_int['start'] > last_int['end']:
+#                 stack.append(intervals[i])
+
+#         return stack
