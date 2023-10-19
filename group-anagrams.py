e01c282537efddf7174872c3d68727a9176ba841
Aurora Ignacio <bellaignacio10@gmail.com>
2023-10-18 20:41:17 -0700

    group anagrams

---
+++ group-anagrams.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for s in strs:
+            sorted_str = ''.join(sorted(s))
+            anagrams[sorted_str].append(s)
+
+        return anagrams.values()
+
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         anagrams = {}
+
+#         for i in range(len(strs)):
+#             sorted_str = ''.join(sorted(strs[i]))
+#             if sorted_str not in anagrams:
+#                 anagrams[sorted_str] = []
+#             anagrams[sorted_str].append(strs[i])
+
+#         return anagrams.values()
