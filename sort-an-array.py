31bc3e7b698b3b9414d5deb6cfa3ce6843fdc30d
Aurora Ignacio <bellaignacio10@gmail.com>
2023-10-19 16:44:59 -0700

    sort an array

---
+++ sort-an-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) < 2:
+            return nums
+
+        mid = len(nums) // 2
+        left = self.sortArray(nums[:mid])
+        right = self.sortArray(nums[mid:])
+
+        return self.merge_sort(left, right)
+
+    def merge_sort(self, left, right):
+        merged = []
+
+        while len(left) and len(right):
+            if left[0] < right[0]:
+                merged.append(left.pop(0))
+            else:
+                merged.append(right.pop(0))
+
+        return merged + left + right
