e19167e2b920d9a2d7c4b60adac6d465ac693b21
Aurora Ignacio <bellaignacio10@gmail.com>
2023-10-18 19:45:10 -0700

    set matrix zeroes

---
+++ set-matrix-zeroes.py
@@ -0,0 +1,37 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row_set = set()
+        col_set = set()
+
+        # find out rows/columns that originally contain 0
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col] == 0:
+                    row_set.add(row)
+                    col_set.add(col)
+
+        # rewrite all values in those rows/columns with 0
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if row in row_set or col in col_set:
+                    matrix[row][col] = 0
+
+
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+#         row_visited = set()
+#         col_visited = set()
+
+#         for row in range(len(matrix)):
+#             for col in range(len(matrix[0])):
+#                 if row not in row_visited and col not in col_visited and matrix[row][col] == 0:
+#                     matrix[row][:] = [0] * len(matrix[0])
+#                     matrix[:][col] = [0] * len(matrix) # populate column with 0's - not working
+#                     row_visited.add(row)
+#                     col_visited.add(col)
