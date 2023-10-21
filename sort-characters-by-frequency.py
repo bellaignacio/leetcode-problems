8ff3e041e1c3e725b53fdf5bb03dc47724d3ca2f
Aurora Ignacio <bellaignacio10@gmail.com>
2023-10-21 14:59:45 -0700

    added example

--- sort-characters-by-frequency.py
+++ sort-characters-by-frequency.py
@@ -1,23 +1,24 @@
-# from collections import defaultdict
 from collections import Counter
 
 class Solution:
     def frequencySort(self, s: str) -> str:
-        # frequencies = defaultdict(int)
-        # for c in s:
-        #     frequencies[c] += 1
-
-        n  = len(s)
-        frequencies = Counter(s)
-        bucket = [[] for _ in range(n + 1)]
+        n  = len(s) # n = 6
+        frequencies = Counter(s) # {e: 1, b: 2, c: 3}
+        buckets = [[] for _ in range(n + 1)] # [[], [], [], [], [], [], []]
 
         for c, f in frequencies.items():
-            bucket[f].append(c)
+            buckets[f].append(c) # [[], [e], [b], [c], [], [], []]
 
         result = ''
 
-        for f in range(n, 0, -1):
-            for char in bucket[f]:
+        for f in range(n, 0, -1): # counting backwards from n to 1
+            for char in buckets[f]:
                 result += f * char
+                # f = n = 6 --> nothing
+                # f = 5 --> nothing
+                # f = 4 --> nothing
+                # f = 3 --> 3 * 'c' --> 'ccc'
+                # f = 2 --> 2 * 'b' --> 'cccbb'
+                # f = 1 --> 1 * 'e' --> 'cccbbe'
 
         return result
