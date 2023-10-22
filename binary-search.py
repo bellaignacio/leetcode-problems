61ee93f5d0c1d1817e0ed4ab500a772109183935
Aurora Ignacio <bellaignacio10@gmail.com>
2023-10-21 18:04:51 -0700

    binary search

---
+++ binary-search.py
@@ -0,0 +1,32 @@
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         left = 0
+#         right = len(nums) - 1
+
+#         while left <= right:
+#             mid = ((right - left) // 2) + left
+#             if nums[mid] == target:
+#                 return mid
+#             elif target < nums[mid]:
+#                 right = mid - 1
+#             elif target > nums[mid]:
+#                 left = mid + 1
+
+#         return -1
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def recurse(left, right):
+            if left > right:
+                return -1
+
+            mid = ((right - left) // 2) + left
+            if nums[mid] == target:
+                return mid
+            elif target < nums[mid]:
+                return recurse(left, mid - 1)
+            elif target > nums[mid]:
+                return recurse(left + 1, right)
+
+        return recurse(0, len(nums) - 1)
