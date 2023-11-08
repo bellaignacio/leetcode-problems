f826296d5545dabbf04f4f97cfc91cb8c416fff3
Aurora Ignacio <bellaignacio10@gmail.com>
2023-11-08 15:26:53 -0800

    k closest points to origin

--- k-closest-points-to-origin.py
+++ k-closest-points-to-origin.py
@@ -1,3 +1,13 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        
+        distances = []
+        for x, y in points:
+            distances.append([(x ** 2) + (y ** 2), x, y]) # don't need to square root - can still compare distances accurately
+        heapq.heapify(distances)
+
+        results = []
+        for i in range(k):
+            dist, x, y = heapq.heappop(distances)
+            results.append([x, y])
+
+        return results
