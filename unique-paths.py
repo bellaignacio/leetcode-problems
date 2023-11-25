f038fe54f3390b37d4ab383c5dfcd6ae9dec3300
Aurora Ignacio <bellaignacio10@gmail.com>
2023-11-24 23:10:25 -0800

    unique paths

--- unique-paths.py
+++ unique-paths.py
@@ -1,20 +1,31 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
+        dp = [[1] * n for _ in range(m)]
 
-        def path_count(m, n):
-            if m == 0 or n == 0:
-                return 0
-            elif m == 1 and n == 1:
-                return 1
+        for col in range(1, m):
+            for row in range(1, n):
+                dp[col][row] = dp[col - 1][row] + dp[col][row - 1]
 
-            key = f'({m}, {n})'
-            if key in memo:
-                return memo[key]
-            memo[key] = path_count(m - 1, n) + path_count(m, n - 1)
-            return memo[key]
+        return dp[-1][-1]
 
-        return path_count(m, n)
+
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         memo = {}
+
+#         def path_count(m, n):
+#             if m == 0 or n == 0:
+#                 return 0
+#             elif m == 1 and n == 1:
+#                 return 1
+
+#             key = f'({m}, {n})'
+#             if key in memo:
+#                 return memo[key]
+#             memo[key] = path_count(m - 1, n) + path_count(m, n - 1)
+#             return memo[key]
+
+#         return path_count(m, n)
 
 
 # class Solution:
