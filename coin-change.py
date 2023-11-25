f7a4a2e8355433a7f4541d9ee51f20b5b78cf1fd
Aurora Ignacio <bellaignacio10@gmail.com>
2023-11-25 08:36:06 -0800

    coin change

--- coin-change.py
+++ coin-change.py
@@ -1,20 +1,33 @@
-from collections import defaultdict
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        def _dfs(remainder, memo):
-            if remainder == 0:
-                return 0
-            if remainder in memo:
-                return memo[remainder]
+        dp = [float('inf') for _ in range(amount + 1)]
+        dp[0] = 0
 
-            memo[remainder] = float('inf')
+        for value in range(1, amount + 1):
             for coin in coins:
-                if remainder - coin >= 0:
-                    memo[remainder] = min(memo[remainder], _dfs(remainder - coin, memo) + 1)
+                if value - coin >= 0:
+                    dp[value] = min(dp[value], dp[value - coin] + 1)
 
-            return memo[remainder]
+        return -1 if dp[amount] == float('inf') else dp[amount]
 
-        memo = defaultdict(int)
-        result = _dfs(amount, memo)
-        return result if result != float('inf') else -1
+
+# from collections import defaultdict
+
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         def _dfs(remainder, memo):
+#             if remainder == 0:
+#                 return 0
+#             if remainder in memo:
+#                 return memo[remainder]
+
+#             memo[remainder] = float('inf')
+#             for coin in coins:
+#                 if remainder - coin >= 0:
+#                     memo[remainder] = min(memo[remainder], _dfs(remainder - coin, memo) + 1)
+
+#             return memo[remainder]
+
+#         memo = defaultdict(int)
+#         result = _dfs(amount, memo)
+#         return result if result != float('inf') else -1
