76cbc01f5b1daedda793a20f6d9e55b2e8a2098c
Aurora Ignacio <bellaignacio10@gmail.com>
2023-12-07 11:42:59 -0800

    find words

---
+++ find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        total = 0
+        chars_count = Counter(chars)
+
+        for word in words:
+            word_count = Counter(word)
+            valid = True
+
+            for c in word_count:
+                if c not in chars_count or chars_count[c] < word_count[c]:
+                    valid = False
+                    break
+
+            if valid:
+                total += len(word)
+
+        return total
